0c96356e9a9827d074df05e69e20dc535638e87d
team2053tigertronics <team2053tigertronics@gmail.com>
2015-01-09 18:01:21 -0500

    added winch control command

--- FODRobot2015/src/Subsystems/Winch.cpp
+++ FODRobot2015/src/Subsystems/Winch.cpp
@@ -1,16 +1,17 @@
 #include "Winch.h"
 #include "../RobotMap.h"
+#include "../Commands/WinchControl.h"
 
-Winch::Winch() : Subsystem("ExampleSubsystem")
+Winch::Winch() : Subsystem("Winch")
 {
 	winchJag = RobotMap::winchJag;
 }
 
 void Winch::InitDefaultCommand()
 {
-	// Set the default command for a subsystem here.
-	//SetDefaultCommand(new MySpecialCommand());
+	SetDefaultCommand(new WinchControl());
 }
 
 // Put methods for controlling this subsystem
 // here. Call these from Commands.
+
