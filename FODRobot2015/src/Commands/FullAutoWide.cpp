e22c8a7837328735a07e77de5d538fd2f03c3ca8
team2053tigertronics <team2053tigertronics@gmail.com>
2015-01-15 19:00:27 -0500

    FullAutoWide zero calls

--- FODRobot2015/src/Commands/FullAutoWide.cpp
+++ FODRobot2015/src/Commands/FullAutoWide.cpp
@@ -16,48 +16,59 @@ FullAutoWide::FullAutoWide()
 
 	//drives back from first tote
 	AddSequential(new driveAuto(0.0, 0.5, 0.0, 0.0, 0.5));
+	AddParallel(new driveAuto(0.0, 0.0, 0.0, 0.0, 0.0));
 
 	//raises winch to second level...
 	AddParallel(new WinchAuto(0.5, 1));
+	AddParallel(new WinchAuto(0.0, 0.0));
 
 	//...while driving to second tote
 	AddSequential(new driveAuto(1, 0.0, 0.0, 0.0, 1.5));
+	AddParallel(new driveAuto(0.0, 0.0, 0.0, 0.0, 0.0));
 
 	//drives forward to second tote
 	AddSequential(new driveAuto(0.0, -0.5, 0.0, 0.0, 0.5));
+	AddParallel(new driveAuto(0.0, 0.0, 0.0, 0.0, 0.0));
 
 	//drops first tote on second tote
 	AddSequential(new PneumaticsAutoOpen(true, true));
 
 	//lowers winch to first level
 	AddSequential(new WinchAuto(-0.5, 1));
+	AddParallel(new WinchAuto(0.0, 0.0));
 
 	//grabs second tote
 	AddSequential(new PneumaticsAutoClose(true, true));
 
 	//drives backwards from second tote
 	AddSequential(new driveAuto(0.0, 0.5, 0.0, 0.0, 0.5));
+	AddParallel(new driveAuto(0.0, 0.0, 0.0, 0.0, 0.0));
 
 	//raise winch to second level...
 	AddParallel(new WinchAuto(0.5, 1));
+	AddParallel(new WinchAuto(0.0, 0.0));
 
 	//...while driving sideways
 	AddSequential(new driveAuto(1, 0.0, 0.0, 0.0, 1.5));
+	AddParallel(new driveAuto(0.0, 0.0, 0.0, 0.0, 0.0));
 
 	//drives forward to second tote
 	AddSequential(new driveAuto(0.0, -0.5, 0.0, 0.0, 0.5));
+	AddParallel(new driveAuto(0.0, 0.0, 0.0, 0.0, 0.0));
 
 	//drops first tote stack on third tote
 	AddSequential(new PneumaticsAutoOpen(true, true));
 
 	//lowers winch to first level
 	AddSequential(new WinchAuto(-0.5, 1));
+	AddParallel(new WinchAuto(0.0, 0.0));
 
 	//grabs third tote
 	AddSequential(new PneumaticsAutoClose(true, true));
 
 	//drives backwards
 	AddSequential(new driveAuto(0.0, 0.5, 0.0, 0.0, 2));
+	AddParallel(new driveAuto(0.0, 0.0, 0.0, 0.0, 0.0));
 
 	//drops totes
 	AddSequential(new PneumaticsAutoOpen(true, true));
