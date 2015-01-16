7f16297dfa9a6479ec16067edf24507cfc908b5c
Team 2053 Tigertronics <team2053tigertronics@gmail.com>
2015-01-16 16:57:10 -0500

    changed fullautowide to driveWhileWinching
    YMCA! WOW

--- FODRobot2015/src/Commands/FullAutoWide.cpp
+++ FODRobot2015/src/Commands/FullAutoWide.cpp
@@ -4,6 +4,7 @@
 #include "PneumaticsAutoOpen.h"
 #include "WinchAuto.h"
 #include "RotatetoAngle.h"
+#include "DriveWhileWinching.h"
 
 FullAutoWide::FullAutoWide()
 {
@@ -14,61 +15,53 @@ FullAutoWide::FullAutoWide()
 	//grabs first tote
 	AddSequential(new PneumaticsAutoClose(true, true));
 
-	//drives back from first tote
-	AddSequential(new driveAuto(0.0, 0.5, 0.0, 0.0, 0.5));
-	AddParallel(new driveAuto(0.0, 0.0, 0.0, 0.0, 0.0));
+	//drives back from first tote while winching
+	AddSequential(new DriveWhileWinching(0.0, 0.5, 0.0, 0.0, 0.5, 0.5, 1));
+	AddSequential(new DriveWhileWinching(0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0));
 
-	//raises winch to second level...
-	AddParallel(new WinchAuto(0.5, 1));
-	AddParallel(new WinchAuto(0.0, 0.0));
-
-	//...while driving to second tote
+	//driving to second tote
 	AddSequential(new driveAuto(1, 0.0, 0.0, 0.0, 1.5));
-	AddParallel(new driveAuto(0.0, 0.0, 0.0, 0.0, 0.0));
+	AddSequential(new driveAuto(0.0, 0.0, 0.0, 0.0, 0.0));
 
 	//drives forward to second tote
 	AddSequential(new driveAuto(0.0, -0.5, 0.0, 0.0, 0.5));
-	AddParallel(new driveAuto(0.0, 0.0, 0.0, 0.0, 0.0));
+	AddSequential(new driveAuto(0.0, 0.0, 0.0, 0.0, 0.0));
 
 	//drops first tote on second tote
 	AddSequential(new PneumaticsAutoOpen(true, true));
 
 	//lowers winch to first level
 	AddSequential(new WinchAuto(-0.5, 1));
-	AddParallel(new WinchAuto(0.0, 0.0));
+	AddSequential(new WinchAuto(0.0, 0.0));
 
 	//grabs second tote
 	AddSequential(new PneumaticsAutoClose(true, true));
 
-	//drives backwards from second tote
-	AddSequential(new driveAuto(0.0, 0.5, 0.0, 0.0, 0.5));
-	AddParallel(new driveAuto(0.0, 0.0, 0.0, 0.0, 0.0));
+	//drives backwards from second tote while winching
+	AddSequential(new DriveWhileWinching(0.0, 0.5, 0.0, 0.0, 0.5, 0.5, 1));
+	AddSequential(new DriveWhileWinching(0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0));
 
-	//raise winch to second level...
-	AddParallel(new WinchAuto(0.5, 1));
-	AddParallel(new WinchAuto(0.0, 0.0));
-
-	//...while driving sideways
+	//drives to third tote
 	AddSequential(new driveAuto(1, 0.0, 0.0, 0.0, 1.5));
-	AddParallel(new driveAuto(0.0, 0.0, 0.0, 0.0, 0.0));
+	AddSequential(new driveAuto(0.0, 0.0, 0.0, 0.0, 0.0));
 
-	//drives forward to second tote
+	//drives forward to third tote
 	AddSequential(new driveAuto(0.0, -0.5, 0.0, 0.0, 0.5));
-	AddParallel(new driveAuto(0.0, 0.0, 0.0, 0.0, 0.0));
+	AddSequential(new driveAuto(0.0, 0.0, 0.0, 0.0, 0.0));
 
 	//drops first tote stack on third tote
 	AddSequential(new PneumaticsAutoOpen(true, true));
 
 	//lowers winch to first level
 	AddSequential(new WinchAuto(-0.5, 1));
-	AddParallel(new WinchAuto(0.0, 0.0));
+	AddSequential(new WinchAuto(0.0, 0.0));
 
 	//grabs third tote
 	AddSequential(new PneumaticsAutoClose(true, true));
 
 	//drives backwards
 	AddSequential(new driveAuto(0.0, 0.5, 0.0, 0.0, 2));
-	AddParallel(new driveAuto(0.0, 0.0, 0.0, 0.0, 0.0));
+	AddSequential(new driveAuto(0.0, 0.0, 0.0, 0.0, 0.0));
 
 	//drops totes
 	AddSequential(new PneumaticsAutoOpen(true, true));
