c3073b70dcc46bc9cdc90bdc3d4eaf6c5f2e6d99
Team 2053 Tigertronics <team2053tigertronics@gmail.com>
2015-03-14 13:08:38 -0400

    working can and tote auto
    wow. that can actaully stayed on.

--- FODRobot2015/src/Commands/CanandToteAuto.cpp
+++ FODRobot2015/src/Commands/CanandToteAuto.cpp
@@ -3,6 +3,7 @@
 #include "WinchAuto.h"
 #include "PneumaticsAuto.h"
 #include "Commands/SetOffSet.h"
+#include "Commands/RotatetoAngle.h"
 #include "DriveAutoCorrect.h"
 
 CanandToteAuto::CanandToteAuto()
@@ -10,12 +11,14 @@ CanandToteAuto::CanandToteAuto()
 	//AddSequential(new SetOffSet(0));//set off set to 0
 	AddSequential(new PneumaticsAuto(true, true, false));//close  arms
 	AddSequential(new WinchAuto(1, 1));//lift winch
-	AddSequential(new DriveAutoCorrect(-0.75,0.0,0.0,Robot::driveBaseSub->getAdjYaw(),0.9,0));//drive left
-	AddSequential(new DriveAutoCorrect(0,-0.4,0,Robot::driveBaseSub->getAdjYaw(), 0.4, 0));
+	AddSequential(new DriveAutoCorrect(0,-0.5,0.0,Robot::driveBaseSub->getAdjYaw(),0.4,0));//drive fow
+	AddSequential(new WinchAuto(-.5, .3));
 	AddSequential(new PneumaticsAuto(true, true, true));//open arms
-	AddSequential(new WinchAuto(-1, 1.6));//lower winch
+	AddSequential(new WinchAuto(-1, 1.5));//lower winch
 	AddSequential(new PneumaticsAuto(true, true, false));//close  arms
-	AddSequential(new DriveAutoCorrect(0.0,0.5,0.0,Robot::driveBaseSub->getAdjYaw(),2.0,0));//drive back
-	AddSequential(new SetOffSet(180));//set off set to 180
+	AddSequential(new WinchAuto(1,.5));
+	AddSequential(new RotatetoAngle(-90, 1));
+	AddSequential(new DriveAutoCorrect(-0.75,-0.1,0.0,Robot::driveBaseSub->getAdjYaw(),1.5,-90));//drive side
+	AddSequential(new SetOffSet(90));//set off set to 90
 
 }
